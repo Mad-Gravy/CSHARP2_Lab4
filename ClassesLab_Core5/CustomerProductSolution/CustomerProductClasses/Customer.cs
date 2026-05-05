5d7bf831c9b4899afd02d154d96374863b0efdb4
Joseph Teague <graveytea@gmail.com, teaguej@my.lanecc.edu>
2026-05-05 16:17:19 -0700

    Finished the Customer Class assignment!

--- ClassesLab_Core5/CustomerProductSolution/CustomerProductClasses/Customer.cs
+++ ClassesLab_Core5/CustomerProductSolution/CustomerProductClasses/Customer.cs
@@ -4,5 +4,64 @@ namespace CustomerProductClasses
 {
     public class Customer
     {
+        private int Id;
+        private string FName;
+        private string LName;
+        private string EMail;
+        private string PhoneNumber;
+
+        public Customer() { } // Default constructor!
+
+        public Customer(int id, string fName, string lName, string eMail, string phoneNumber) //Full constructor!
+        {
+            Id = id;
+            FName = fName;
+            LName = lName;
+            EMail = eMail;
+            PhoneNumber = phoneNumber;
+        }
+
+
+        // Getters and Setters!
+        public int CustomerId
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+
+        public string FirstName
+        {
+            get { return FName; }
+            set { FName = value; }
+        }
+
+        public string LastName
+        {
+            get { return LName; }
+            set { LName = value; }
+        }
+
+        public string Email
+        {
+            get { return EMail; }
+            set{ EMail = value; }
+        }
+
+        public string Phone
+        {
+            get { return PhoneNumber; }
+            set { PhoneNumber = value; }
+        }
+
+
+        // ToString Method!
+        public override string ToString()
+        {
+            return String.Format(
+                "Id: {0} First Name: {1} Last Name: {2} Email: {3} Phone: {4}", 
+                Id, FName, LName, EMail, PhoneNumber);
+        }
+
+        // This was much easier than trying to teach recursive code to play the Tower of Hanoi game lol
     }
 }
